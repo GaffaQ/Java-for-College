fbb402fca2d8cd7f2ca731d255c38c6108158b20
Gaptixa <gaptixdev@gmail.com>
2025-09-25 15:21:52 +0700

    new

---
+++ src/array/hitungRataRata.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class hitungRataRata {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int total,n;
+        n = in.nextInt();
+        int[] arr = new int[n];
+
+        //input isi array
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        } 
+
+        //hitung rata rata
+        total = 0;
+        for (int i = 0; i < n; i++) {
+            total += arr[i];
+        }
+
+        System.out.println("rata rata: " + total/n);
+
+    }
+    
+}
